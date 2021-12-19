8ba8aa39d78b27f9cccd4fa5c4333c54b6183c96
stefanitg <96338913+stefanitg@users.noreply.github.com>
2021-12-19 12:47:55 +0300

    Update test_tz3.py

--- test_tz3.py
+++ test_tz3.py
@@ -16,18 +16,18 @@ def random_list():
 def test_search_maxn():
     file_test = random_list()
     maxi = max(file_test)
-    assert search_maxn(file_test) == maxi
+    assert search_maxn(file_test) == float(maxi)
 
 
 def test_search_minn():
     file_test = random_list()
     mini = max(file_test)
-    assert search_minn(file_test) == mini
+    assert search_minn(file_test) == float(mini)
 
 
 def test_find_sum_of_n():
     file_test = random_list()
-    file_test_fsum = list(file_test)
+    file_test_fsum = file_test.split()
     sumi = sum(file_test_fsum)
     assert find_sum_of_n(file_test) == sumi
 
@@ -35,11 +35,12 @@ def test_find_sum_of_n():
 def test_find_multi():
     multi = 1
     file_test = random_list()
-    file_test_fmult = list(file_test)
-    for i in file_test_fmult:
+    file_for_multi = file_test.split()
+    for i in file_for_multi:
         multi *= float(i)
     assert find_multi(file_test) == multi
 
 
 def test_is_digits():
-    assert type(search_maxn(random_list())) == float
+    digit = search_maxn(random_list())
+    assert type(digit) == float or type(digit) == int
